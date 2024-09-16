838c9cebebb766407763f514a7daeb23571448f8
Renz <rcs@zimaone.com>
2024-09-17 07:42:12 +0800

    refactor: notification label

--- composeApp/src/commonMain/kotlin/link/limecode/reddit/lite/DesktopRoot.kt
+++ composeApp/src/commonMain/kotlin/link/limecode/reddit/lite/DesktopRoot.kt
@@ -1,26 +1,32 @@
 package link.limecode.reddit.lite
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import link.limecode.reddit.lite.desktop.DesktopNavigation
 import link.limecode.reddit.lite.desktop.DesktopNavigationRoutes
 import link.limecode.reddit.lite.desktop.ui.components.RedditDesktopAppBar
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun DesktopRoot() {
     val navController = rememberNavController()
 
     Scaffold(
         topBar = {
-            RedditDesktopAppBar(
-                navToHome = { navController.navigate(DesktopNavigationRoutes.HOME.route) },
-                navToChat = { navController.navigate(DesktopNavigationRoutes.CHAT.route) },
-                navToNotification = { navController.navigate(DesktopNavigationRoutes.NOTIFICATION.route) },
-                navToProfile = { navController.navigate(DesktopNavigationRoutes.PROFILE.route) }
-            )
+            if (navController.currentDestination?.route != DesktopNavigationRoutes.LOGIN.route) {
+                RedditDesktopAppBar(
+                    navToHome = { navController.navigate(DesktopNavigationRoutes.HOME.route) },
+                    navToChat = { navController.navigate(DesktopNavigationRoutes.CHAT.route) },
+                    navToNotification = { navController.navigate(DesktopNavigationRoutes.NOTIFICATION.route) },
+                    navToProfile = { navController.navigate(DesktopNavigationRoutes.PROFILE.route) },
+                    navToLogin = { navController.navigate(DesktopNavigationRoutes.LOGIN.route) }
+                )
+            }
         },
         modifier = Modifier.fillMaxSize()
     ) {
