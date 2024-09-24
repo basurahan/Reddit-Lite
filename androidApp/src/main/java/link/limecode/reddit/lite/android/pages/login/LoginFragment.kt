0eed489b3922d9913cddfb18adb614dc79e54cff
Renz <rcs@zimaone.com>
2024-09-24 08:02:31 +0800

    feature: login flow

--- androidApp/src/main/java/link/limecode/reddit/lite/android/pages/login/LoginFragment.kt
+++ androidApp/src/main/java/link/limecode/reddit/lite/android/pages/login/LoginFragment.kt
@@ -1,9 +1,12 @@
 package link.limecode.reddit.lite.android.pages.login
 
 import android.os.Bundle
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.ScrollView
 import link.limecode.reddit.lite.android.databinding.FragmentLoginBinding
 import link.limecode.reddit.lite.presentation.viewmodel.login.AndroidLoginViewModel
 import link.limecode.vuebinder.FragmentViewBinding
@@ -14,19 +17,37 @@ class LoginFragment : FragmentViewBinding<FragmentLoginBinding>() {
     private val viewModel: AndroidLoginViewModel by viewModel()
 
     override fun bind(inflater: LayoutInflater, container: ViewGroup?): FragmentLoginBinding {
-        return FragmentLoginBinding.inflate(inflater, container, false)
+        val binding = FragmentLoginBinding.inflate(inflater, container, false)
+        with(binding) {
+            val displayMetrics = resources.displayMetrics
+            val screenHeight = displayMetrics.heightPixels
+
+            val typedValue = TypedValue()
+            requireActivity().theme.resolveAttribute(android.R.attr.actionBarSize, typedValue, true)
+            val actionBarSize = TypedValue.complexToDimensionPixelSize(typedValue.data, displayMetrics)
+            val desiredHeight = screenHeight - actionBarSize
+
+            val params = groupForm.layoutParams as FrameLayout.LayoutParams
+            params.height = desiredHeight
+            groupForm.layoutParams = params
+            groupForm.requestLayout()
+            groupForm.invalidate()
+        }
+        return binding
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         with(viewBinding) {
-            button.setOnClickListener {
+
+
+            /*button.setOnClickListener {
                 viewModel.login(
                     username = "dev.renz",
                     password = "S@langa1998"
                 )
-            }
+            }*/
         }
     }
 }
