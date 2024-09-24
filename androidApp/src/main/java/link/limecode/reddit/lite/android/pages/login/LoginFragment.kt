c7d00d557632fee7bfcc9c202ab84ffb418598f7
Renz <dev.renzsalanga@gmail.com>
2024-09-24 14:37:39 +0800

    feature: android done login page design

--- androidApp/src/main/java/link/limecode/reddit/lite/android/pages/login/LoginFragment.kt
+++ androidApp/src/main/java/link/limecode/reddit/lite/android/pages/login/LoginFragment.kt
@@ -4,18 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.FrameLayout
-import androidx.coordinatorlayout.widget.CoordinatorLayout
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.fragment.findNavController
 import link.limecode.reddit.lite.android.databinding.FragmentLoginBinding
+import link.limecode.reddit.lite.android.util.BaseKeyboardSafeCoordinatorLayout
 import link.limecode.reddit.lite.android.util.showSoftKeyboardFor
 import link.limecode.reddit.lite.presentation.viewmodel.login.AndroidLoginViewModel
-import link.limecode.vuebinder.FragmentViewBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class LoginFragment : FragmentViewBinding<FragmentLoginBinding>() {
+class LoginFragment : BaseKeyboardSafeCoordinatorLayout<FragmentLoginBinding>() {
 
     private val viewModel: AndroidLoginViewModel by viewModel()
 
@@ -27,28 +23,6 @@ class LoginFragment : FragmentViewBinding<FragmentLoginBinding>() {
         super.onViewCreated(view, savedInstanceState)
 
         with(viewBinding) {
-            ViewCompat.setOnApplyWindowInsetsListener(parent) { view, insets ->
-                val coordinator = view as CoordinatorLayout
-                val imeHeight = insets.getInsets(WindowInsetsCompat.Type.ime()).bottom
-
-                val param = coordinator.layoutParams as FrameLayout.LayoutParams
-                if (param.bottomMargin != imeHeight) {
-                    param.setMargins(
-                        param.leftMargin,
-                        param.topMargin,
-                        param.rightMargin,
-                        imeHeight
-                    )
-                    coordinator.layoutParams = param
-                }
-
-                // somehow appbar layout is not in sync with the changes of the layout
-                // so we gonna notify it manually
-                coordinator.dispatchDependentViewsChanged(appbarLayout)
-
-                insets
-            }
-
             toolbar.setNavigationOnClickListener {
                 findNavController().navigateUp()
             }
