edd1103b1a40e7fd2df7eb5cf61efd31bc66f93c
Renz <rcs@zimaone.com>
2024-10-12 11:08:17 +0800

    feature: ios event bus implementation

---
+++ iosApp/iosApp/Presentation/AppViewController.swift
@@ -0,0 +1,14 @@
+//
+//  RootViewController.swift
+//  iosApp
+//
+//  Created by Apple on 10/12/24.
+//  Copyright © 2024 orgName. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UINavigationController {
+    
+    let sessionViewModel = SessionViewModel()
+}
