ba9ea26342227422fed26623a14d7f3a002a5f9b
Renz <rcs@zimaone.com>
2024-10-13 10:50:05 +0800

    fix: swift ui preview does not work after linking sql delight

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -31,7 +31,7 @@ kotlin {
         podfile = project.file("../iosApp/Podfile")
         framework {
             baseName = "shared"
-            isStatic = true
+            isStatic = false
         }
     }
     
