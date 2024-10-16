e908374901e2ace4208c3674bbe1b882a19efb04
Renz <rcs@zimaone.com>
2024-10-16 18:45:49 +0800

    draft: initial profile page design

--- iosApp/iosApp/Presentation/Home/Profile/ProfileView.swift
+++ iosApp/iosApp/Presentation/Home/Profile/ProfileView.swift
@@ -10,11 +10,121 @@ import UIKit
 
 class ProfileView: UIView {
 
-    lazy var button: UIButton = {
-        let button = UIButton(type: .system)
-        button.setTitle("Login", for: .normal)
+    lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.delaysContentTouches = false
+        scrollView.addSubview(container)
+        
+        NSLayoutConstraint.activate([
+            container.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            container.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            container.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            container.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            container.widthAnchor.constraint(equalTo: scrollView.widthAnchor)
+        ])
+        
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
+    }()
+    
+    lazy var container: UIView = {
+        let view = UIView()
+        view.addSubview(avatar)
+        view.addSubview(username)
+        view.addSubview(editInformation)
+        view.addSubview(divider)
+        view.addSubview(aboutTitle)
+        view.addSubview(aboutBody)
+        
+        NSLayoutConstraint.activate([
+            avatar.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            avatar.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            avatar.heightAnchor.constraint(equalToConstant: 200),
+            avatar.widthAnchor.constraint(equalToConstant: 200),
+            
+            username.topAnchor.constraint(equalTo: avatar.bottomAnchor, constant: 24),
+            username.centerXAnchor.constraint(equalTo: avatar.centerXAnchor),
+            
+            editInformation.topAnchor.constraint(equalTo: username.bottomAnchor, constant: 16),
+            editInformation.centerXAnchor.constraint(equalTo: avatar.centerXAnchor),
+            
+            divider.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            divider.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            divider.topAnchor.constraint(equalTo: editInformation.bottomAnchor, constant: 16),
+            divider.heightAnchor.constraint(equalToConstant: 1),
+            
+            aboutTitle.topAnchor.constraint(equalTo: divider.bottomAnchor, constant: 16),
+            aboutTitle.centerXAnchor.constraint(equalTo: avatar.centerXAnchor),
+            
+            aboutBody.topAnchor.constraint(equalTo: aboutTitle.bottomAnchor, constant: 16),
+            aboutBody.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            aboutBody.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            aboutBody.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -24)
+        ])
+        
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    lazy var avatar: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = UIImage(named: "Avatar")
+        imageView.layer.cornerRadius = 100
+        imageView.layer.masksToBounds = true
+        imageView.layer.borderColor = UIColor.lightGray.cgColor
+        imageView.layer.borderWidth = 1
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    lazy var username: UILabel = {
+        let label = UILabel()
+        label.text = "dev.renz"
+        label.font = UIFont.preferredFont(forTextStyle: .title1)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var editInformation: UIButton = {
+        var config = UIButton.Configuration.filled()
+        config.title = "Edit Information"
+        config.cornerStyle = .capsule
+        config.baseBackgroundColor = .systemBlue
+        config.baseForegroundColor = .white
+        config.image = UIImage(systemName: "pencil")
+        config.imagePlacement = .leading
+        config.imagePadding = 8
+        config.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20)
+        
+        let button = UIButton(configuration: config)
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
+    }()
+    
+    lazy var divider: UIView = {
+        let view = UIView()
+        view.backgroundColor = .lightGray
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    lazy var aboutTitle: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .title1)
+        label.text = "About this app"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var aboutBody: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .body)
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.text = "Hey, I’m Renz Carlo Salanga, your friendly neighborhood fullstack developer. I built this app to prove that Kotlin Multiplatform is so awesome, even Chuck Norris would switch from roundhouse kicks to coding. 😄"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
     }()
     
     override init(frame: CGRect) {
@@ -28,13 +138,13 @@ class ProfileView: UIView {
     }
     
     private func setupView() {
-        // Add the horizontal stack view to the main view
-        self.addSubview(button)
+        self.addSubview(scrollView)
         
-        // Set up constraints
         NSLayoutConstraint.activate([
-            button.centerXAnchor.constraint(equalTo: self.centerXAnchor),
-            button.centerYAnchor.constraint(equalTo: self.centerYAnchor)
+            scrollView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            scrollView.topAnchor.constraint(equalTo: self.topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
         ])
     }
 }
