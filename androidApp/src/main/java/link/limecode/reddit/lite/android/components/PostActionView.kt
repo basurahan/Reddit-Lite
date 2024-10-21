99204843e62a536ae305f1ae4aa7a2b7a3f7e0b3
Renz <rcs@zimaone.com>
2024-10-21 16:34:16 +0800

    draft: item post

---
+++ androidApp/src/main/java/link/limecode/reddit/lite/android/components/PostActionView.kt
@@ -0,0 +1,18 @@
+package link.limecode.reddit.lite.android.components
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import com.google.android.material.card.MaterialCardView
+import link.limecode.reddit.lite.android.databinding.ViewPostActionBinding
+
+class PostActionView @JvmOverloads constructor(
+    context: Context,
+    attribSet: AttributeSet? = null,
+    defStyleRef: Int = com.google.android.material.R.attr.materialCardViewOutlinedStyle
+) : MaterialCardView(context, attribSet, defStyleRef) {
+
+    init {
+        ViewPostActionBinding.inflate(LayoutInflater.from(context), this, true)
+    }
+}
